58455dc147d2e9142601662ec6144915f2432d56
johnny <jbui923@gmail.com>
2020-07-15 01:52:55 -0700

    minor adjustment

--- src/components/Feeduser.js
+++ src/components/Feeduser.js
@@ -6,7 +6,7 @@ const FeedUser = props => {
     return (
         <div className='feedUser-container'>
             <div className='feedUser__icon'> <img src='https://avatars3.githubusercontent.com/u/60685723?s=400&u=8906bc06047d2374252904e393c4acb11358be6d&v=4'/> </div>
-            <div className='feedUser__user'>johnny.bui</div>
+            <div className='feedUser__user'>johnny</div>
         </div>
     )
 }
