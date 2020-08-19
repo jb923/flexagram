e5186c5eb745e8c4d60448bf638bf6833dab41f8
johnny <jbui923@gmail.com>
2020-08-19 10:51:52 -0700

    footer

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -10,9 +10,14 @@ const LandingPage = props => {
         <div className="landing__page">
             <div className="landing__image">
                 <img className="login__image" src={require("../assets/iphone.png")} alt="landing-img"/>
-                <img className="inner__image" src={require("../assets/greece.jpg")} alt="inner-landing"/>
+                <img className="inner__image" src={require("../assets/umbrella.jpg")} alt="inner-landing"/>
             </div>
             <Login {...props}/>
+            {/* <div className="demo2__container">
+                <p>Example Login: </p>
+                <p>email: ceejayduhh@ceejayduhh.com</p>
+                <p>pw: ceejayduhh</p>
+            </div> */}
         </div>
     )
 }
